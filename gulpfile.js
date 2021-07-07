472447341a95383ab684d305f656c9340f5661a9
Cory Kelley <coryk0789@gmail.com>
2021-07-07 12:31:01 -0400

    updated scss to css and gulp tasks to minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 const gulp = require('gulp');
-const themeKit = require('@shopify/themekit');
-const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const themeKit = require('@shopify/themekit');
 
-gulp.task('sass', function () {
+gulp.task('minify', () => {
 	return gulp
-		.src('./assets/scss/application.scss')
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.src('./assets/styles/application.css')
 		.pipe(clean({ compatibility: 'ie11' }))
+		.pipe(
+			rename({
+				basename: 'bundle',
+				suffix: '.min',
+			})
+		)
 		.pipe(gulp.dest('./assets'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./assets/scss/*.scss', gulp.series('sass'));
+	gulp.watch('./assets/styles/application.css', gulp.series('minify'));
 	themeKit.command('watch', {
 		allowLive: true,
 		env: 'development',
